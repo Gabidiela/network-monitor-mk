68118d1317e6fa1c5e292f03af6703685a439098
ti-larabrenda <ti.larabrenda@gmail.com>
2025-06-11 19:28:09 -0300

    Add files via upload

--- frontend/assets/script.js
+++ frontend/assets/script.js
@@ -21,9 +21,11 @@
   // Chart setup
   const ctx = document.getElementById('bwChart').getContext('2d');
   const data = { labels: [], datasets: [
-    { label: 'Rx (Mbps)', data: [], borderColor: '#1E90FF', fill: false, tension: 0 },
-    { label: 'Tx (Mbps)', data: [], borderColor: '#32CD32', fill: false, tension: 0 }
+    { label: 'Rx (Mbps)', data: [], borderColor: '#E91E63	', fill: false, tension: 0 },
+    { label: 'Tx (Mbps)', data: [], borderColor: '#8E24AA', fill: false, tension: 0 }
   ]};
+
+  const maxPoints = 3 * 60;
 
   const chart = new Chart(ctx, {
     type: 'line',
@@ -32,12 +34,34 @@
       animation: { duration: 0 },
       scales: {
         x: {
-          type: 'time', time: { unit: 'second', displayFormats: { second: 'HH:mm:ss' } },
-          grid: { display: false }
+          type: 'time',
+          time: {
+            // define que o “unit” é minuto
+            unit: 'minute',
+            // espaçamento de 1 unidade (1 minuto)
+            stepSize: 2,
+            // formato de exibição do label
+            displayFormats: {
+              minute: 'HH:mm'
+            }
+          },
+          ticks: {
+            // faz o Chart gerar ticks automaticamente seguindo seu unit/stepSize
+            source: 'auto',
+            autoSkip: true,
+            maxTicksLimit: 50
+          },
+          grid: { display: true }
         },
-        y: { beginAtZero: true, grid: { display: false } }
+        y: {
+          beginAtZero: true,
+          grid: { display: true }
+        }
       },
-      plugins: { tooltip: { enabled: true, mode: 'nearest', intersect: false }, legend: { position: 'top' } },
+      plugins: {
+        tooltip: { enabled: true, mode: 'nearest', intersect: false },
+        legend: { position: 'top' }
+      },
       layout: { padding: { top: 10, bottom: 10 } }
     }
   });
@@ -57,15 +81,15 @@
   socket.on('connect', () => { statusEl.textContent = 'Conectado'; });
   socket.on('disconnect', () => { statusEl.textContent = 'Desconectado'; });
 
-  socket.on('bandwidth', ({ download, upload }) => {
-    console.log('>> bandwidth:', payload);
+  socket.on('bandwidth', ({ download, upload, delta_in, delta_out, interval, unit }) => {
+    console.log(`bytes in: ${delta_in}, bytes out: ${delta_out}, dt: ${interval}s`);
     if (paused) return;
     const now = Date.now();
     lastTimeEl.textContent = new Date(now).toLocaleTimeString();
     data.labels.push(now);
     data.datasets[0].data.push(download);
     data.datasets[1].data.push(upload);
-    if (data.labels.length > 30) {
+    if (data.labels.length > maxPoints) {
       data.labels.shift(); data.datasets.forEach(ds => ds.data.shift());
     }
     chart.update('none');
